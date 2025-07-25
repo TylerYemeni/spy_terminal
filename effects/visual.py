bb4c378549810a608327ab04d2f989a815aa5e1f
TylerYemeni <leomessi111909@gmail.com>
2025-07-26 01:23:58 +0300

    Create visual.py

---
+++ effects/visual.py
@@ -0,0 +1,10 @@
+import time
+import os
+
+def visual_noise():
+    for i in range(5):
+        os.system("clear")
+        print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+        print("⚠️ تشويش بصري جاري...")
+        print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+        time.sleep(0.5)
